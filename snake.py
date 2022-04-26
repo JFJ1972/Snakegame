c439d08e35f5349d9de161e2a43060df50ebf340
JFJ1972 <97111500+JFJ1972@users.noreply.github.com>
2022-04-26 12:54:14 -0500

    Animation snakee

--- snake.py
+++ snake.py
@@ -1,5 +1,6 @@
-
-from turtle import Screen, Turtle
+from hashlib import new
+from turtle import Screen, Turtle, pen, penup
+import time
 
 #Crear escenario
 
@@ -8,15 +9,42 @@ screen.setup(width=600, height=600) # X, Y
 screen.bgcolor( "black")
 screen.title("Programate")
 
-#ahora el cuerpo
-positions = [(0,0),(-20, 0),(-40, 0)]
+screen.tracer(0)#se quita animacion de cuadro x cuadro
 
-for position in positions:
+#ahora el cuerpo
+starting_positions = [(0,0),(-20, 0),(-40, 0)]
+
+segments = []
+
+for position in starting_positions:
     snake_segment = Turtle("square")
     snake_segment.color("white")
+    snake_segment.penup()#quita la linea
     snake_segment.goto(position)
+    segments.append(snake_segment)
 
-''''
+game_is_on = True
+
+while game_is_on:   
+    screen.update()#refresca la pantalla para que se vean los cuadros continuos
+    time.sleep(0.2)#tiempo de movimiento
+
+    '''
+            for segment in segments:
+        segment.forward(20)
+    '''
+    for seg_num in range(len(segments) - 1, 0, -1 ):
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+
+    segments[0].forward(20)
+    segments[0].left(90) 
+
+
+
+
+'''
 snake_segment = Turtle("square")
 snake_segment.color("white")
 
@@ -28,5 +56,7 @@ snake_segment3 = Turtle("square")
 snake_segment3.color("white")
 snake_segment3.goto(position)
 '''
+
+
 
 screen.exitonclick()
